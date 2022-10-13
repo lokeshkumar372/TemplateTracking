f1f3aa8028b67cffd172f99fe315a741d4111c74
Lokesh Sepuri <lokesh.sepuri@techigai.local>
2022-10-13 10:39:30 +0530

    DataOperations Code Changes

--- src/main/java/com/DataOperations.java
+++ src/main/java/com/DataOperations.java
@@ -5,14 +5,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-<<<<<<< HEAD:src/main/java/com/DataOperations.java
-public class DataOperations {
-=======
 import com.bean.Users;
 
-public class Repo {
->>>>>>> 324c080a69022d73491493a30f1766b13cae8371:src/main/java/com/Repo.java
+public class DataOperations {
+
 
 	private static Statement stmt = null;
 	private static Connection con = null;
@@ -49,11 +45,8 @@ public class Repo {
 		return k;
 	}
 
-<<<<<<< HEAD:src/main/java/com/DataOperations.java
-	public ResultSet getUserDetails(String select) throws SQLException {
-=======
 	public ResultSet getUsersDetails(String select) throws SQLException {
->>>>>>> 324c080a69022d73491493a30f1766b13cae8371:src/main/java/com/Repo.java
+
 
 		String sql = "select user_name,user_email from Users where role = '" + select + "' ;";
 		ResultSet res = stmt.executeQuery(sql);
@@ -70,3 +63,4 @@ public class Repo {
 	}
 
 }
+
